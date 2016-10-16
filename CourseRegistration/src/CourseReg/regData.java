88d3c0571565261ca2cfa94f18de5bdc73eb2a13
RidwanBRACU <ridwan.ramanujon@gmail.com>
2016-10-16 23:51:41 +0600

    course registration

---
+++ CourseRegistration/src/CourseReg/regData.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CourseReg;
+
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Ridwan
+ */
+public class regData {
+    public static TreeSet name = new TreeSet();
+    public static TreeSet id = new TreeSet();
+    public static  TreeSet email = new TreeSet();
+    public static  TreeSet pass = new TreeSet();
+    public static boolean b = false;
+    public static TreeSet users = new TreeSet();
+        
+}
